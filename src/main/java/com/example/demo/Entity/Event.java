b4d6f45543f0a8bfc5cb1e2559070a73f0307844
MartaOsuchaMazur <jagooo1@wp.pl>
2025-09-06 13:23:59 +0200

    added dependencies to Event Entity

--- src/main/java/com/example/demo/Entity/Event.java
+++ src/main/java/com/example/demo/Entity/Event.java
@@ -1,14 +1,13 @@
 package com.example.demo.Entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Data
 @Entity
@@ -17,11 +16,18 @@ public class Event {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @NotBlank
+    @NotBlank (message = "Name cannot be blank")
     private String name;
     private String description;
     private LocalDate date;
-    @Min(1)
+    @Min(value = 1, message = "Capacity must be at least 1")
     private int capacity;
+
+    @ManyToOne
+    @JoinColumn(name = "location_id")
+    private Location location;
+
+    @OneToMany(mappedBy = "event", cascade = CascadeType.ALL)
+    private Set<Registration> registrations = new HashSet<>();;
 
 }
