f8a41b30b93cb7a32d77b7f8b59602feef713f62
MartaOsuchaMazur <jagooo1@wp.pl>
2025-09-06 14:48:46 +0200

    added meddage to Participant's Entity

--- src/main/java/com/example/demo/Entity/Participant.java
+++ src/main/java/com/example/demo/Entity/Participant.java
@@ -1,12 +1,12 @@
 package com.example.demo.Entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Data
 @Entity
@@ -15,9 +15,14 @@ public class Participant {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @NotBlank
+
+    @NotBlank (message = "Name cannot be blank")
     private String name;
-    @Email
-    @NotBlank
+
+    @Email (message = "Invalid email")
+    @NotBlank (message = "Email cannot be blank")
     private String email;
+
+    @OneToMany(mappedBy = "participant", cascade = CascadeType.ALL)
+    private Set<Registration> registrations = new HashSet<>();
 }
