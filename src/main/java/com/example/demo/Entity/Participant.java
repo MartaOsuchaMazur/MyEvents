ab5897d4cb5171e6235f802b15233392527edece
MartaOsuchaMazur <jagooo1@wp.pl>
2025-09-06 13:09:11 +0200

    added Participant's Entity

--- src/main/java/com/example/demo/Entity/Participant.java
+++ src/main/java/com/example/demo/Entity/Participant.java
@@ -4,18 +4,20 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 @Entity
-public class Location {
+public class Participant {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @NotBlank
     private String name;
+    @Email
     @NotBlank
-    private String city;
-    private String address;
+    private String email;
 }
