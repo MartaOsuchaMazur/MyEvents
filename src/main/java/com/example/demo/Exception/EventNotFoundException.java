e5bb8e719fa17c825955be76e4dce613433f8c22
MartaOsuchaMazur <jagooo1@wp.pl>
2025-09-07 16:06:52 +0200

    added Event not foun exception

--- src/main/java/com/example/demo/Exception/EventNotFoundException.java
+++ src/main/java/com/example/demo/Exception/EventNotFoundException.java
@@ -1,4 +1,8 @@
 package com.example.demo.Exception;
 
-public class EventNotFoundException {
+public class EventNotFoundException extends RuntimeException {
+
+    public EventNotFoundException(Long id) {
+        super("Event not found with id: " + id);
+    }
 }
