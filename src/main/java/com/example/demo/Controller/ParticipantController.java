4c0a4c85f5cad8a7903ecdc49b61ed89aea3c0f1
MartaOsuchaMazur <jagooo1@wp.pl>
2025-09-07 16:02:22 +0200

    added participant controller

--- src/main/java/com/example/demo/Controller/ParticipantController.java
+++ src/main/java/com/example/demo/Controller/ParticipantController.java
@@ -1,4 +1,26 @@
 package com.example.demo.Controller;
 
+import com.example.demo.Entity.Participant;
+import com.example.demo.Service.ParticipantService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/participants")
 public class ParticipantController {
+
+    private final ParticipantService participantService;
+
+    public ParticipantController(ParticipantService participantService) {
+        this.participantService = participantService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Participant> create(@Valid @RequestBody Participant p) {
+        return ResponseEntity.ok(participantService.save(p));
+    }
 }
