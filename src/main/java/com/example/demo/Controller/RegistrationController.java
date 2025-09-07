43ffb60cee9cd70d515b8aab4e08da5cdadce78a
MartaOsuchaMazur <jagooo1@wp.pl>
2025-09-07 16:03:32 +0200

    added registration  controller

--- src/main/java/com/example/demo/Controller/RegistrationController.java
+++ src/main/java/com/example/demo/Controller/RegistrationController.java
@@ -1,4 +1,22 @@
 package com.example.demo.Controller;
 
+import com.example.demo.Entity.Registration;
+import com.example.demo.Service.RegistrationService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/registrations")
 public class RegistrationController {
+
+    private final RegistrationService registrationService;
+
+    public RegistrationController(RegistrationService registrationService) {
+        this.registrationService = registrationService;
+    }
+
+    @PostMapping("/events/{eventId}")
+    public ResponseEntity<Registration> register(@RequestParam String email, @PathVariable Long eventId) {
+        return ResponseEntity.ok(registrationService.register(email, eventId));
+    }
 }
