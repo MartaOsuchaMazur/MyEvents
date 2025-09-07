f04a94bb095b417fa8313cd82426a10e1b3a28c6
MartaOsuchaMazur <jagooo1@wp.pl>
2025-09-07 16:01:09 +0200

    added Event Controller

--- src/main/java/com/example/demo/Controller/EventController.java
+++ src/main/java/com/example/demo/Controller/EventController.java
@@ -1,4 +1,42 @@
 package com.example.demo.Controller;
 
+import com.example.demo.Entity.Event;
+import com.example.demo.Service.EventService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.ui.Model;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/events")
 public class EventController {
+
+    private final EventService eventService;
+
+    public EventController(EventService eventService) {
+        this.eventService = eventService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Event>> eventList() {
+        return ResponseEntity.ok(eventService.getEvents());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Event> getEvent(@PathVariable Long id) {
+        return ResponseEntity.ok(eventService.getEvent(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<Event> createEvent(@Valid @RequestBody Event event) {
+        return ResponseEntity.ok(eventService.addEvent(event));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteEvent(@PathVariable Long id){
+        eventService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 }
